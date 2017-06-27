85708d301d04969bc684de5be9bc2fd3012f72ef
Oliver Howarth <oah5@njit.edu>
2017-06-27 19:36:44 -0400

    added function to add categoies

---
+++ add_category.php
@@ -0,0 +1,16 @@
+<?php
+$name = filter_input(INPUT_POST, 'name');
+
+if ($name == null) {
+    $error = "Invalid.";
+    include('error.php');
+} else {
+    require_once('database.php');
+    $query = 'INSERT INTO categories (categoryName) VALUES (:category_name)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_name', $name);
+    $statement->execute();
+    $statement->closeCursor();
+    include('category_list.php');
+}
+?>
